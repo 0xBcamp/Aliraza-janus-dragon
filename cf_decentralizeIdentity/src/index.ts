e46bd5cb85a82b78c73f5d000d99f343d379ad12
Muhammad-Masood <development.masood@gmail.com>
2024-02-05 04:09:36 -0800

    ipfs_storage

--- cf_decentralizeIdentity/src/index.ts
+++ cf_decentralizeIdentity/src/index.ts
@@ -1,35 +1,27 @@
 import { DecentralizeIdentity } from "./interfaces/DI";
 
 // sample credential
-const universityCredential: {
-  name: string;
-  degree: string;
-  program: string;
-  date: Date;
-  did: string;
-} = {
+const universityCredential: string = JSON.stringify({
   name: "Stanford University",
   degree: "Masters",
   program: "Computer Science",
-  date: new Date(),
-  did: "did:university:d104ff2d80c041e91259a9e567b554f359d6986ab95e672cbcc1be16e9c4961a"
-}
+  date: new Date().toISOString(),
+  did: "did:university:d104ff2d80c041e91259a9e567b554f359d6986ab95e672cbcc1be16e9c4961a",
+});
 
 const main = async () => {
   console.log("Server running on port 3000...");
   const di: DecentralizeIdentity = new DecentralizeIdentity();
   const wallet_address: string = di.contract_init.getWallet().address;
   console.log(`User: ${wallet_address}`);
-  // const issuer_did: string = di.createIdentifier(
-  //   "university",
-  //   wallet_address
-  // );
-  // const holder_did: string = di.createIdentifier(
-  //   "education",
-  //   "0x672BeB69B7129762fB2847bdA5f73E75029c9349"
-  //   );
-  // console.log(`Generated Local Identifier: ${issuer_did}`);
-  // di.issueCredential(universityCredential, issuer_did, holder_did);
+  const issuer_did: string = di.createIdentifier("university", wallet_address);
+  const holder_did: string = di.createIdentifier(
+    "education",
+    "0x672BeB69B7129762fB2847bdA5f73E75029c9349"
+  );
+  console.log(`Issuer DID: ${issuer_did}`);
+  console.log(`Holder DID: ${holder_did}`);
+  // await di.issueCredential(universityCredential, issuer_did, holder_did);
   // console.log(`Holder Decentralize Identifier: ${holder_did}`);
   // const holder_address: string = "0x672BeB69B7129762fB2847bdA5f73E75029c9349";
   // await decentralizedIdentity.storeDID(issuer_did, wallet.address);
--- cf_decentralizeIdentity/src/interfaces/DI.ts
+++ cf_decentralizeIdentity/src/interfaces/DI.ts
@@ -1,71 +1,87 @@
 import { createHash } from "crypto";
 import { BaseContract, HDNodeWallet } from "ethers";
-import { DecentralizeIdentityContractInit, DIContractInterface } from "./Contract";
+import {
+  DecentralizeIdentityContractInit,
+  DIContractInterface,
+} from "./Contract";
 import { ThirdwebStorage } from "@thirdweb-dev/storage";
+import dotenv from "dotenv";
+dotenv.config();
+
+const storage = new ThirdwebStorage({
+  secretKey: process.env.IPFS_STORAGE_KEY,
+});
 
 export class DecentralizeIdentity {
-    contract_init: DecentralizeIdentityContractInit;
-    contract: DIContractInterface;
+  contract_init: DecentralizeIdentityContractInit;
+  contract: DIContractInterface;
 
-    constructor() {
-        this.contract_init = new DecentralizeIdentityContractInit();
-        this.contract = this.contract_init.getContract();
-        const _helia_init = async () => {
-        }
-        _helia_init();
-    }
-  
-    /**
-     * @param address - wallet address of the user
-     * @param name - name of the identifier
-     * @returns decentralize identifier
-     */
-    createIdentifier = (name: string, address: string): string => {
-      const hashable: string = name + address;
-      const did: string =
-        `did:${name}:` + createHash("sha256").update(hashable).digest("hex");
-      return did;
-    };
-  
-    /**
-     * @dev This function stores the DID in the wallet of the user on the blockchain
-     * @param did decentralize identifier
-     * @param address wallet address of user
-     */
-    storeDID = async (did: string, address: string) => {
-      try{
-        console.log(`Storing DID {${did}} on address {${address}}...`);
-        await this.contract.assignDID(address, did);
-        console.log(`ONCHAIN => Identity assigned successfully.`);
-    } catch(error){
-        console.log(error);
-      }
-    }
-  
-    /**
-     * @notice This function do double verification of the did on both OnChain and OffChain.
-     * @param did decentralize identifier of the user to verify
-     * @param address public key/wallet address
-     * @returns verification check (TRUE or FALSE)
-     */
-    verifyDID = async (did: string, address: string): Promise<boolean | undefined> => {
-      try{
-        console.log("Verifying Identity...");
-        const user_dids: string[] = await this.contract.getDIDs(address);
-        const isDidOnChainVerified: boolean = user_dids.includes(did);
-        const name: string = did.split(":")[1];
-        const expected_did:string = this.createIdentifier(name, address);
-        return did === expected_did && isDidOnChainVerified;
-      } catch(error){
-        console.log(error);
-      }
-    };
-  
-    issueCredential = async (credential: any,issuer_did: string, holder_did: string) => {
-      // get credential hash
-      // console.log(`Issued Credential: CID => ${cid}`);
-      // return cid;
-    }
-  
-    verifyCredential = async () => {}
+  constructor() {
+    this.contract_init = new DecentralizeIdentityContractInit();
+    this.contract = this.contract_init.getContract();
   }
+
+  /**
+   * @param address - wallet address of the user
+   * @param name - name of the identifier
+   * @returns decentralize identifier
+   */
+  createIdentifier = (name: string, address: string): string => {
+    const hashable: string = name + address;
+    const did: string =
+      `did:${name}:` + createHash("sha256").update(hashable).digest("hex");
+    return did;
+  };
+
+  /**
+   * @dev This function stores the DID in the wallet of the user on the blockchain
+   * @param did decentralize identifier
+   * @param address wallet address of user
+   */
+  storeDID = async (did: string, address: string) => {
+    try {
+      console.log(`Storing DID {${did}} on address {${address}}...`);
+      await this.contract.assignDID(address, did);
+      console.log(`ONCHAIN => Identity assigned successfully.`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * @notice This function do double verification of the did on both OnChain and OffChain.
+   * @param did decentralize identifier of the user to verify
+   * @param address public key/wallet address
+   * @returns verification check (TRUE or FALSE)
+   */
+  verifyDID = async (
+    did: string,
+    address: string
+  ): Promise<boolean | undefined> => {
+    try {
+      console.log("Verifying Identity...");
+      const user_dids: string[] = await this.contract.getDIDs(address);
+      const isDidOnChainVerified: boolean = user_dids.includes(did);
+      const name: string = did.split(":")[1];
+      const expected_did: string = this.createIdentifier(name, address);
+      return did === expected_did && isDidOnChainVerified;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  issueCredential = async (
+    credential: string,
+    issuer_did: string,
+    holder_did: string
+  ): Promise<string> => {
+    // get credential hash
+    const ipfs_cid: string = await storage.upload(credential);
+    const cid: string = ipfs_cid.split("/")[2];
+    // await this.contract.issueCredentials(updated_credential);
+    console.log(`Issued Credential: CID => ${cid}`);
+    return cid;
+  };
+
+  verifyCredential = async () => {};
+}
